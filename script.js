81e06bf40a9b1187d3ba56273c9b205a60956b9d
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2022-12-29 11:04:18 -0300

    05. Manipulando formulários

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 async function buscaEndereco(cep) {
+  var mensagemErro = document.getElementById("erro");
+  mensagemErro.innerHTML = "";
   try {
     var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     var consultaCEPConvertida = await consultaCEP.json();
     if (consultaCEPConvertida.erro) {
       throw Error("CEP não existente!");
     }
-    console.log(consultaCEPConvertida);
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var estado = document.getElementById("estado");
+
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+
     return consultaCEPConvertida;
   } catch (erro) {
+    mensagemErro.innerHTML = `<br><p>Cep inválido. Tente novamente!</p>`;
     console.log(erro);
   }
 }
@@ -15,3 +25,6 @@ async function buscaEndereco(cep) {
 // let ceps = ["01001000", "01001001"];
 // let conjuntoCeps = ceps.map((valores) => buscaEndereco(valores));
 // Promise.all(conjuntoCeps).then((respostas) => console.log(respostas));
+
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
